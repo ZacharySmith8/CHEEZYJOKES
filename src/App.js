553c1ececaf6020d39e55f3ebfa6999fff968018
SatoruSukehiro <zlsmith1014@gmail.com>
2022-02-28 14:34:12 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import JokeList from "./JokeList";
+
+class App extends Component {
+  render() {return (
+    <div className="App">
+      <JokeList />
+    </div>
+  );
+  }
+}
+
+export default App;
